836f7ee24a47a4be2c2fe8003788f7529377dc0d
sechirkov <sergechircov@yandex.ru>
2016-09-12 11:08:55 +0300

    revert foldLeft future

--- src/main/scala/com/sch/library/dao/LibraryDatabase.scala
+++ src/main/scala/com/sch/library/dao/LibraryDatabase.scala
@@ -15,9 +15,6 @@ import scala.concurrent.{ExecutionContext, Future}
  * Date: 9/8/2016
  */
 object LibraryDatabase extends DB {
-  type TableName = String
-  type DBAction = slick.dbio.DBIOAction[_, slick.dbio.NoStream, _ <:Effect]
-
   lazy val listOfBook = List(
     Book(None, "Programming in Scala Second Edition", "Martin Odersky, Lex Spoon, Bill Venners", Some("asdf431g")),
     Book(None, "Scala By Example", "Martin Odersky", Some("qwert54y")),
@@ -30,29 +27,17 @@ object LibraryDatabase extends DB {
   )
 
   def init()(implicit executionContext: ExecutionContext) = {
-    val actions = new ArrayBuffer[(TableName, DBAction)]
-    actions += (("book", DBIO.seq(
+    checkTableExistenceAndDoAction("book", db.run(DBIO.seq(
       books.schema.create,
       books ++= listOfBook
-    )))
-    actions += (("user", DBIO.seq(
-      users.schema.create,
-      users ++= userList
-    )))
-
-    actions.foldLeft[Future[_]](Future { })((f, a) => f andThen { case _ => checkTableExistenceAndDoAction(a._1, db.run(a._2)) }) andThen { case _ => db.close() }
-
-    /*checkTableExistenceAndDoAction("book", db.run(DBIO.seq(
-      books.schema.create,
-      books ++= listOfBook
-    ))) andThen { case _ =>
+    ))) flatMap { case _ =>
       checkTableExistenceAndDoAction("user", db.run(DBIO.seq(
         users.schema.create,
         users ++= userList
       ))) andThen {
         case _ => db.close()
       }
-    }*/
+    }
   }
 
   private def checkTableExistenceAndDoAction(tableName: String, action: => Unit)(implicit executionContext: ExecutionContext): Future[_] = {
